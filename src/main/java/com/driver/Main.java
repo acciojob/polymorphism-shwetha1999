781775c02a0784ec22b88e6b4f65015efd752410
shwetha1999 <shwethadevang@gmail.com>
2023-01-14 19:15:01 +0530

    Third commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,7 +12,7 @@ public class Main {
     }
 public static class Product{
     public int product(int x, int y) {
-        return x+y;
+        return x*y;
     }
     public int product(int x, int y, int z) {
         return x*y*z;
