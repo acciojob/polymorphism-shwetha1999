fb88a8913152b4d2f3ba8fe7615f2d68c17f8a73
shwetha1999 <shwethadevang@gmail.com>
2023-01-14 16:42:45 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        Product p = new Product();
+        p.product(10, 10);
+        p.product(10, 10, 10);
+        p.product(10.00, 10.00);
 
+
+    }
+public static class Product{
+    public int product(int x, int y) {
+        return x+y;
+    }
+    public int product(int x, int y, int z) {
+        return x*y*z;
+    }
+
+    public double product(double x, double y) {
+        return x*y;
+    }
+
+}
 }
